d449ee7505c2e558bec63e98a6e94dcf69fb168a
luciano <crucciluciano@gmail.com>
2023-05-22 11:32:59 -0300

    primer1

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -21,7 +21,7 @@ formProducts.onsubmit = (e) => {
         title: inputTitle.value,
         description: inputDescript.value,
         price: +inputPrice.value,
-        thumbnail: inputThumbnail.value,
+        picture: inputThumbnail.value,
         code: inputCode.value,
         stock: +inputStock.value,
         category: inputCategory.value,
@@ -34,7 +34,20 @@ formProducts.onsubmit = (e) => {
 
 socket.on("listProdSocke", listProdSocke => {
     window.location.reload();
-});
+    // const html = listProdSocke.map((element) => {
+    //     return (`
+    //         <tr>
+    //             <td><h4>${element.title}</h4></td>
+    //             <td><h4>${element.price}</h4></td>
+    //             <td><img src="${element.thumbnail}" class="rounded" width="75"/></td>
+    //         </tr>
+    //     `);
+    // }).join(" ");
+    // document.getElementById('productListUpdated').innerHTML = html;
+    // socket.emit('products-shown', 'Productos mostrados');
+})
+
+
 
 formeliminar.onsubmit = (e) => {
     e.preventDefault();
